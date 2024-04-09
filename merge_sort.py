b8a94ea7c302d61bd9bde75540a28f92e1ccc2d1
leo-carrey <leo.carrey@laplateforme.io>
2024-04-09 11:29:11 +0200

    ADD quick_sort

--- merge_sort.py
+++ merge_sort.py
@@ -8,16 +8,13 @@ def merge_sort(arr):
     if len(arr) <= 1:
         return arr
     
-    # Diviser le tableau en deux parties égales
     mid = len(arr) // 2
     left = arr[:mid]
     right = arr[mid:]
     
-    # Tri récursif des sous-tableaux
     left_sorted = merge_sort(left)
     right_sorted = merge_sort(right)
     
-    # Fusion des sous-tableaux triés
     return merge(left_sorted, right_sorted)
 
 def merge(left, right):
@@ -32,7 +29,6 @@ def merge(left, right):
             result.append(right[right_idx])
             right_idx += 1
     
-    # Ajouter les éléments restants
     result.extend(left[left_idx:])
     result.extend(right[right_idx:])
     
@@ -49,8 +45,29 @@ def bubble_sort(arr):
         i += 1
     return arr
 
-arr = [38, 27, 43, 3, 9, 82, 10]
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr.pop()
+    print(pivot,"pivot")
+    lower_than_pivot = []
+    higher_than_pivot = []
+    for number in arr:
+        if number < pivot:
+            lower_than_pivot.append(number)
+            print(lower_than_pivot,"petit")
+        else:
+            higher_than_pivot.append(number)
+            print(higher_than_pivot,"grand")
+    return quick_sort(lower_than_pivot)+[pivot]+quick_sort(higher_than_pivot)
+
+
+
+
+arr = [38, 27, 43, 42, 12, 3, 9, 82, 10]
 array = [1,2,3,4,5]
 
-print(merge_sort(arr))
-print(bubble_sort(arr))
+# print(merge_sort(arr))
+# print(bubble_sort(arr))
+print(quick_sort(arr))
